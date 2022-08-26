a17e241aee17a051c35a47441204d575e7e623fd
mujeebcc <mujeeb@codingcrust.com>
2022-08-26 12:27:35 +0500

    Search screen completed

--- lib/screens/home_screens/search_screen.dart
+++ lib/screens/home_screens/search_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-
 import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
 
+import '../../helpers/routes.dart';
 import '../../widgets/const_widgets/search_input_field.dart';
+
+import '../../../providers/categories_provider/main_categories_provider.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final mainCategoryData =
+        Provider.of<MainCategoriesProvider>(context, listen: false);
+    final extractMainCategory = mainCategoryData.mainCategories;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -21,13 +27,61 @@ class SearchScreen extends StatelessWidget {
           "Search_Screen_AppBar_Title",
           style: Theme.of(context).textTheme.bodyLarge,
         ).tr(),
-        bottom:const PreferredSize(
+        bottom: const PreferredSize(
           preferredSize: Size.fromHeight(60),
           child: SearchInputField(),
         ),
       ),
       body: SingleChildScrollView(
-        child:Column() ,
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              ListView.builder(
+                padding: const EdgeInsets.only(
+                    left: 0.0, top: 10.0, right: 0.0, bottom: 10.0),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: extractMainCategory!.length,
+                itemBuilder: (ctx, index) => InkWell(
+                  onTap: () => Navigator.of(context).pushNamed(
+                    MyRoutes.SUBCATEGORYROUTE,
+                    arguments: {
+                      'id': index,
+                    },
+                  ),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.only(
+                        left: 0.0, bottom: 10.0, top: 10.0, right: 0.0),
+                    leading: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        // color: Colors.green,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          '${MyRoutes.IMAGEURL}/${extractMainCategory[index].image}',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      extractMainCategory[index].title,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    trailing: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
