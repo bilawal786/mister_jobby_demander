0834625deee476d79583dd90eefd2ba6e7eb9c56
Hassan <hassan@codingcrust.com>
2022-08-29 17:23:37 +0500

    little changes

---
+++ lib/screens/home_screens/services_sub_categories/process_steps_screens/general_step_2_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GeneralStep2Screen extends StatelessWidget {
+  const GeneralStep2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
