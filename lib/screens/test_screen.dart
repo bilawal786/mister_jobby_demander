b0d7826ad0fc08a6457c9f254b24b733d3450c2b
Hassan <hassan@codingcrust.com>
2022-09-03 22:50:24 +0500

    add child screen

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mister_jobby/widgets/const_widgets/custom_button.dart';
+import 'package:provider/provider.dart';
 
-import '../widgets/home_screen_widgets/service_sub_categories/process_const_widgets/group_radio_tile.dart';
-
+import '../providers/const_provider/const_provider.dart';
 
 class TestScreen extends StatefulWidget {
   const TestScreen({Key? key}) : super(key: key);
@@ -11,7 +12,8 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
-  int _gValue = 1;
+  int childValue = 0;
+  String _dropDownValue = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,41 +31,127 @@ class _TestScreenState extends State<TestScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              GroupRadioTile(
-                title: 'title here',
-                subTitle: 'sub title',
-                groupValue: _gValue,
-                value: 1,
-                onClick: (p) {
+              if (childValue > 0)
+                for (int i = 0; i < childValue; ++i)
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Text(
+                              "Boy or Girl",
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  childValue -= 1;
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 40.0,
+                          margin: const EdgeInsets.all(5.0),
+                          //   height: 40,
+                          padding: const EdgeInsets.fromLTRB(20, 10, 0, 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black, width: 1),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: DropdownButtonFormField<String>(
+                            decoration: InputDecoration(
+                              hintText: "Select Gender",
+                              hintStyle: Theme.of(context).textTheme.bodyMedium,
+                              isCollapsed: true,
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                            isExpanded: true,
+                            iconSize: 30.0,
+                            items: [
+                              'Boy',
+                              'Girl',
+                            ].map(
+                              (val) {
+                                return DropdownMenuItem<String>(
+                                  value: val,
+                                  child: Text(
+                                    val,
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                );
+                              },
+                            ).toList(),
+                            onChanged: (val) {
+                              setState(() {
+                                _dropDownValue = val!;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width / 40,
+                        ),
+                        Text(
+                          "Date of Birth",
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width / 40,
+                        ),
+                        Consumer<ConstProvider>(
+                          builder: (_, selectDate, child) => GestureDetector(
+                            onTap: () {
+                              selectDate.selectDateProvider(context);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 30, right: 10),
+                              margin: const EdgeInsets.all(8),
+                              alignment: Alignment.centerLeft,
+                              width: MediaQuery.of(context).size.width,
+                              height: 45,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.2),
+                                    spreadRadius: 0.2,
+                                    blurRadius: 1,
+                                    offset: const Offset(
+                                        0.5, 1), // changes position of shadow
+                                  ),
+                                ],
+                                border: Border.all(color: Colors.grey.shade300),
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: Text(
+                                "${selectDate.selectedDate.day < 10 ? "0${selectDate.selectedDate.day}" : "${selectDate.selectedDate.day}"}-${selectDate.selectedDate.month < 10 ? "0${selectDate.selectedDate.month}" : "${selectDate.selectedDate.month}"}-${selectDate.selectedDate.year}",
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              CustomButton(
+                onPress: () {
                   setState(() {
-                    _gValue = p!;
+                    childValue += 1;
                   });
-                  print(_gValue);
                 },
-              ),
-              GroupRadioTile(
-                title: 'title here',
-                subTitle: 'sub title',
-                groupValue: _gValue,
-                value: 2,
-                onClick: (p) {
-                  setState(() {
-                    _gValue = p!;
-                  });
-                  print(_gValue);
-                },
-              ),
-              GroupRadioTile(
-                title: 'title here',
-                subTitle: 'sub title',
-                groupValue: _gValue,
-                value: 3,
-                onClick: (p) {
-                  setState(() {
-                    _gValue = p!;
-                  });
-                  print(_gValue);
-                },
+                buttonName: "Add a child",
               ),
             ],
           ),
