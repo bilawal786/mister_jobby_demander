8ad86702248e3a4b547817975b26c613427732a7
Your Name <you@example.com>
2022-09-12 00:50:19 -0700

    cours particuliers all screen api added

--- lib/screens/home_screens/services_sub_categories/process_sub_categories_screens/french_screen.dart
+++ lib/screens/home_screens/services_sub_categories/process_sub_categories_screens/french_screen.dart
@@ -25,7 +25,7 @@ class _FrenchScreenState extends State<FrenchScreen> {
   @override
   Widget build(BuildContext context) {
     final routeArgs =
-    ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     mainCateId = routeArgs['mainCategoryId'];
     subCateId = routeArgs['subCategoryId'];
     subCateTitle = routeArgs['subCategoryTitle'];
@@ -78,6 +78,42 @@ class _FrenchScreenState extends State<FrenchScreen> {
               print("latitude : ${constProviderData.latitude}");
               print("Postal Code : ${constProviderData.postalCode}");
               print("work Description : ${constProviderData.workDetails}");
+
+              constProviderData.postJob(
+                mainCateId.toString(),
+                subCateId.toString(),
+                0.toString(),
+                subCateTitle!,
+                constProviderData.selectedDate.toString(),
+                constProviderData.pickedTime.toString(),
+                constProviderData.duration.toString(),
+                constProviderData.hourlyRate.toString(),
+                constProviderData.checkUrgentJob.toString(),
+                constProviderData.providersAmount.toString(),
+                constProviderData.estimateBudge.toString(),
+                constProviderData.completeAddress,
+                constProviderData.longitude.toString(),
+                constProviderData.latitude.toString(),
+                constProviderData.postalCode,
+                constProviderData.countryDropDownValue,
+                constProviderData.workDetails,
+                constProviderData.smallSizedFurnitureAmount.toString(),
+                constProviderData.mediumSizedFurnitureAmount.toString(),
+                constProviderData.largeSizedFurnitureAmount.toString(),
+                constProviderData.veryLargeSizedFurnitureAmount.toString(),
+                constProviderData.educationLevelTitle,
+                constProviderData.fixesAmount.toString(),
+                constProviderData.needWork,
+                constProviderData.courseFormatTitle,
+                constProviderData.frequencyTitle,
+                "",
+                constProviderData.pickupAddress,
+                constProviderData.destinationAddress,
+                constProviderData.areaToMowSliderValue.toString(),
+                constProviderData.imageFile0,
+                constProviderData.imageFile1,
+                constProviderData.imageFile2,
+              );
             } else {
               setState(() => currentStep += 1);
             }
@@ -93,7 +129,9 @@ class _FrenchScreenState extends State<FrenchScreen> {
               child: Consumer<ConstProvider>(
                 builder: (_, need, child) => Row(
                   children: <Widget>[
-                    if (need.educationLevelTrueValue > 0 && need.courseFormatTrueValue > 0 && need.frequencyTrueValue > 0 )
+                     ((currentStep < 1) && (need.educationLevelTrueValue > 0) &&
+                        (need.courseFormatTrueValue > 0) &&
+                        (need.frequencyTrueValue > 0))?
                       Expanded(
                         child: ElevatedButton(
                           onPressed: details.onStepContinue,
@@ -114,7 +152,59 @@ class _FrenchScreenState extends State<FrenchScreen> {
                                 letterSpacing: 1),
                           ).tr(),
                         ),
+                      ):SizedBox(),
+                        ((currentStep == 1) &&
+                          (need.duration > 0) &&
+                          (need.hourlyRate > 0))
+                          ? Expanded(
+                        child: ElevatedButton(
+                          onPressed: details.onStepContinue,
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50.0),
+                            primary: Theme.of(context).primaryColor,
+                            elevation: 5,
+                          ),
+                          child: Text(
+                            currentStep > 1
+                                ? "Process_Screen_Confirm_Button"
+                                : "Process_Screen_Continue_Button",
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Cerebri Sans Regular',
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                letterSpacing: 1),
+                          ).tr(),
+                        ),
+                      )
+                          : const SizedBox(),
+                    ((currentStep == 2) &&
+                        (need.completeAddress != '') &&
+                        (need.postalCode != '') &&
+                        (need.countryDropDownValue != 'null'))
+                        ? Expanded(
+                      child: ElevatedButton(
+                        onPressed: details.onStepContinue,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50.0),
+                          primary: Theme.of(context).primaryColor,
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          currentStep > 1
+                              ? "Process_Screen_Confirm_Button"
+                              : "Process_Screen_Continue_Button",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Cerebri Sans Regular',
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white,
+                              letterSpacing: 1),
+                        ).tr(),
                       ),
+                    )
+                        : const SizedBox(),
+
                     SizedBox(
                       width: MediaQuery.of(context).size.width / 40,
                     ),
@@ -158,7 +248,10 @@ class _FrenchScreenState extends State<FrenchScreen> {
           isActive: currentStep >= 1,
           state: currentStep > 1 ? StepState.complete : StepState.indexed,
           title: const Text(""),
-          content: GeneralStep2Screen(mainCategoryId: mainCateId!, subCategoryId: subCateId!,childCategoryId: 0),
+          content: GeneralStep2Screen(
+              mainCategoryId: mainCateId!,
+              subCategoryId: subCateId!,
+              childCategoryId: 0),
         ),
         Step(
           isActive: currentStep >= 2,
