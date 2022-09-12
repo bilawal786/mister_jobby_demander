b6c8a4b8c33da2a8a7edc115f350876062c1e8fe
Your Name <you@example.com>
2022-09-11 22:50:40 -0700

    vehicle repair and reparation depannage api added

--- lib/screens/home_screens/services_sub_categories/process_sub_categories_screens/troubleshooting_screen.dart
+++ lib/screens/home_screens/services_sub_categories/process_sub_categories_screens/troubleshooting_screen.dart
@@ -74,6 +74,42 @@ class _TroubleShootingScreenState extends State<TroubleShootingScreen> {
               print("latitude : ${constProviderData.latitude}");
               print("Postal Code : ${constProviderData.postalCode}");
               print("work Description : ${constProviderData.workDetails}");
+
+              constProviderData.postJob(
+                mainCateId.toString(),
+                subCateId.toString(),
+                0.toString(),
+                subCateTitle!,
+                constProviderData.selectedDate.toString(),
+                constProviderData.pickedTime.toString(),
+                constProviderData.duration.toString(),
+                constProviderData.hourlyRate.toString(),
+                constProviderData.checkUrgentJob.toString(),
+                constProviderData.providersAmount.toString(),
+                constProviderData.estimateBudge.toString(),
+                constProviderData.completeAddress,
+                constProviderData.longitude.toString(),
+                constProviderData.latitude.toString(),
+                constProviderData.postalCode,
+                constProviderData.countryDropDownValue,
+                constProviderData.workDetails,
+                constProviderData.smallSizedFurnitureAmount.toString(),
+                constProviderData.mediumSizedFurnitureAmount.toString(),
+                constProviderData.largeSizedFurnitureAmount.toString(),
+                constProviderData.veryLargeSizedFurnitureAmount.toString(),
+                constProviderData.groupValue.toString(),
+                constProviderData.fixesAmount.toString(),
+                constProviderData.needWork,
+                constProviderData.jobberRemoveWasteTitle,
+                constProviderData.frequencyTitle,
+                "",
+                constProviderData.pickupAddress,
+                constProviderData.destinationAddress,
+                constProviderData.areaToMowSliderValue.toString(),
+                constProviderData.imageFile0,
+                constProviderData.imageFile1,
+                constProviderData.imageFile2,
+              );
             } else {
               setState(() => currentStep += 1);
             }
@@ -89,7 +125,8 @@ class _TroubleShootingScreenState extends State<TroubleShootingScreen> {
               child: Consumer<ConstProvider>(
                 builder: (_, size, child) => Row(
                   children: <Widget>[
-                    Expanded(
+                    (currentStep < 1)
+                        ? Expanded(
                       child: ElevatedButton(
                         onPressed: details.onStepContinue,
                         style: ElevatedButton.styleFrom(
@@ -109,7 +146,59 @@ class _TroubleShootingScreenState extends State<TroubleShootingScreen> {
                               letterSpacing: 1),
                         ).tr(),
                       ),
-                    ),
+                    )
+                        : const SizedBox(),
+                    ((currentStep == 1) &&
+                        (size.duration > 0) &&
+                        (size.hourlyRate > 0))
+                        ? Expanded(
+                      child: ElevatedButton(
+                        onPressed: details.onStepContinue,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50.0),
+                          primary: Theme.of(context).primaryColor,
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          currentStep > 1
+                              ? "Process_Screen_Confirm_Button"
+                              : "Process_Screen_Continue_Button",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Cerebri Sans Regular',
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white,
+                              letterSpacing: 1),
+                        ).tr(),
+                      ),
+                    )
+                        : const SizedBox(),
+                    ((currentStep == 2) &&
+                        (size.completeAddress != '') &&
+                        (size.postalCode != '') &&
+                        (size.countryDropDownValue != 'null'))
+                        ? Expanded(
+                      child: ElevatedButton(
+                        onPressed: details.onStepContinue,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50.0),
+                          primary: Theme.of(context).primaryColor,
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          currentStep > 1
+                              ? "Process_Screen_Confirm_Button"
+                              : "Process_Screen_Continue_Button",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Cerebri Sans Regular',
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white,
+                              letterSpacing: 1),
+                        ).tr(),
+                      ),
+                    )
+                        : const SizedBox(),
                     SizedBox(
                       width: MediaQuery.of(context).size.width / 40,
                     ),
