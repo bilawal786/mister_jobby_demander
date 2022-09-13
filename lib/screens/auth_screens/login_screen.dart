baae5bc4c5d82197ff4be4459cc01185d5f8efdb
Hassan <hassan@codingcrust.com>
2022-09-13 14:14:15 +0500

    apply toggle on obscure password

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mister_jobby/providers/auth_provider/login_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 
+import '../../providers/auth_provider/login_provider.dart';
 import '../../providers/const_provider/const_provider.dart';
 import '../../providers/country_provider/country_list_provider.dart';
 import '../../widgets/const_widgets/custom_button.dart';
