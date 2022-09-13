a097cf5a01eebfea7f487f5602d1db9de2f70918
Hassan <hassan@codingcrust.com>
2022-09-13 11:51:28 +0500

    login provider and model is created

---
+++ lib/models/auth_model/login_model.dart
@@ -0,0 +1,117 @@
+// To parse this JSON data, do
+//
+//     final loginModel = loginModelFromJson(jsonString);
+
+import 'dart:convert';
+
+LoginModel loginModelFromJson(String str) => LoginModel.fromJson(json.decode(str));
+
+String loginModelToJson(LoginModel data) => json.encode(data.toJson());
+
+class LoginModel {
+  LoginModel({
+    required this.success,
+  });
+
+  Success success;
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
+    success: Success.fromJson(json["success"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "success": success.toJson(),
+  };
+}
+
+class Success {
+  Success({
+    required this.token,
+    required this.user,
+  });
+
+  String token;
+  User user;
+
+  factory Success.fromJson(Map<String, dynamic> json) => Success(
+    token: json["token"],
+    user: User.fromJson(json["user"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "token": token,
+    "user": user.toJson(),
+  };
+}
+
+class User {
+  User({
+    required this.userId,
+    required this.firstName,
+    required this.lastName,
+    required this.phone,
+    required this.email,
+    required this.address,
+    required this.country,
+    required this.postalCode,
+    required this.image,
+    required this.categoryId,
+    required this.subcategoryId,
+    required this.role,
+    required this.gender,
+    required this.description,
+    required this.dob,
+  });
+
+  int userId;
+  String firstName;
+  String lastName;
+  String phone;
+  String email;
+  String address;
+  String country;
+  String postalCode;
+  String image;
+  int categoryId;
+  int subcategoryId;
+  int role;
+  String gender;
+  String description;
+  String dob;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    userId: json["user_id"],
+    firstName: json["firstName"],
+    lastName: json["lastName"],
+    phone: json["phone"],
+    email: json["email"],
+    address: json["address"],
+    country: json["country"],
+    postalCode: json["postalCode"],
+    image: json["image"],
+    categoryId: json["category_id"],
+    subcategoryId: json["subcategory_id"],
+    role: json["role"],
+    gender: json["gender"],
+    description: json["description"],
+    dob: json["dob"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "user_id": userId,
+    "firstName": firstName,
+    "lastName": lastName,
+    "phone": phone,
+    "email": email,
+    "address": address,
+    "country": country,
+    "postalCode": postalCode,
+    "image": image,
+    "category_id": categoryId,
+    "subcategory_id": subcategoryId,
+    "role": role,
+    "gender": gender,
+    "description": description,
+    "dob": dob,
+  };
+}
---
+++ lib/providers/auth_provider/login_provider.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../helpers/routes.dart';
+import '../../models/auth_model/login_model.dart';
+
+class LoginProvider with ChangeNotifier{
+
+
+
+  Future<void> login(email, password, countryId) async {
+    try {
+      var response = await http.post(
+        Uri.parse('${MyRoutes.BASEURL}/login'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        },
+        body:
+        jsonEncode(<String, String>{'email': email, 'password': password, 'role': countryId}),
+      );
+
+      if (response.statusCode == 200) {
+        final login = LoginModel.fromJson(jsonDecode(response.body));
+
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('token', login.success.token);
+        prefs.setString(
+            'name', '${login.success.user.firstName} ${login.success.user.lastName}');
+        prefs.setString('email', login.success.user.email);
+        prefs.setString('photo', login.success.user.image);
+      }
+      else{
+        print("Failed to login.");
+      }
+      print(response.body.toString());
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+}
