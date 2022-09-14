4649e7983dff11e4b8f504aad16139682cd5d928
Hassan <hassan@codingcrust.com>
2022-09-14 11:31:58 +0500

    jobs screens

--- lib/screens/jobs_screens/jobs_screen.dart
+++ lib/screens/jobs_screens/jobs_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-import '../../widgets/const_widgets/custom_button.dart';
+import '../../screens/jobs_screens/jobs_tab_screens/completed_tab.dart';
+import '../../screens/jobs_screens/jobs_tab_screens/in_progress_tab.dart';
+
 
 class JobsScreen extends StatelessWidget {
   const JobsScreen({Key? key}) : super(key: key);
@@ -75,84 +77,10 @@ class JobsScreen extends StatelessWidget {
               ),
             ),
           ],
-          body: TabBarView(
-            children: [
-              SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(40.0),
-                  margin: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      Icon(
-                        Icons.find_in_page_rounded,
-                        size: 150,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      Text(
-                        "Jobs_TapBar_Page1_Title",
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ).tr(),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 40,
-                      ),
-                      Text(
-                        "Jobs_TabBar_Page1_Description",
-                        style: Theme.of(context).textTheme.labelLarge,
-                        textAlign: TextAlign.center,
-                      ).tr(),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 40,
-                      ),
-                      CustomButton(
-                        onPress: () {},
-                        buttonName: "I_need_a_favor",
-                        elevation: 8,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(40.0),
-                  margin: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      Icon(
-                        Icons.find_in_page_rounded,
-                        size: 150,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      Text(
-                        "Jobs_TapBar_Page1_Title",
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ).tr(),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 40,
-                      ),
-                      Text(
-                        "Jobs_TabBar_Page1_Description",
-                        style: Theme.of(context).textTheme.labelLarge,
-                        textAlign: TextAlign.center,
-                      ).tr(),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 40,
-                      ),
-                      CustomButton(
-                        onPress: () {},
-                        buttonName: "I_need_a_favor",
-                        elevation: 8,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          body: const TabBarView(
+            children: <Widget>[
+               InProgressTab(),
+               CompletedTab(),
             ],
           ),
         ),
---
+++ lib/screens/jobs_screens/jobs_tab_screens/in_progress_tab.dart
@@ -0,0 +1,50 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../../widgets/const_widgets/custom_button.dart';
+
+class InProgressTab extends StatelessWidget {
+  const InProgressTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(40.0),
+        margin: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Icon(
+              Icons.find_in_page_rounded,
+              size: 150,
+              color: Theme.of(context).primaryColor,
+            ),
+            Text(
+              "Jobs_TapBar_Page1_Title",
+              style: Theme.of(context).textTheme.titleSmall,
+            ).tr(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 40,
+            ),
+            Text(
+              "Jobs_TabBar_Page1_Description",
+              style: Theme.of(context).textTheme.labelLarge,
+              textAlign: TextAlign.center,
+            ).tr(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 40,
+            ),
+            CustomButton(
+              onPress: () {},
+              buttonName: "I_need_a_favor",
+              elevation: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
