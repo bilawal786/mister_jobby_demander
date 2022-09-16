0c3250c70ac97024c813c8f446b26b03a14d80fb
mujeebcc <mujeeb@codingcrust.com>
2022-09-16 10:17:58 +0500

    added notification screen appbar

--- lib/screens/home_screens/notification_display.dart
+++ lib/screens/home_screens/notification_display.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class NotificationDisplay extends StatefulWidget {
@@ -11,6 +12,13 @@ class _NotificationDisplayState extends State<NotificationDisplay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text("Notifications".tr(),style: Theme.of(context).textTheme.titleSmall,),
+        centerTitle: true,
+         iconTheme: Theme.of(context).iconTheme,
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(15),
@@ -18,22 +26,24 @@ class _NotificationDisplayState extends State<NotificationDisplay> {
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             itemCount: 10,
-            itemBuilder: (ctx, index) => ListTile(
-              title: Text(
-                "7 days ago",
-                style: Theme.of(context).textTheme.labelMedium,
-              ),
-              subtitle: Text(
-                "your job 'Assembly of IKEA furniture' has been closed",
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              trailing: const Icon(
-                Icons.arrow_forward_ios_outlined,
-                size: 20,
-              ),
-              shape: Border(
-                bottom: BorderSide(color: Colors.grey.shade200, width: 1),
-              ),
+            itemBuilder: (ctx, index) => Column(
+              children:<Widget> [
+                ListTile(
+                  title: Text(
+                    "7 days ago",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  subtitle: Text(
+                    "your job 'Assembly of IKEA furniture' has been closed",
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    size: 20,
+                  ),
+                ),
+               const Divider(),
+              ],
             ),
           ),
         ),
