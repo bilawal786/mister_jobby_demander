a4d5f17a6f7acba3792e882ea2f7088e12cf7d6b
mujeebcc <mujeeb@codingcrust.com>
2022-09-16 13:00:37 +0500

    contact screen

--- lib/screens/accounts_screens/useful_information/contact_screen.dart
+++ lib/screens/accounts_screens/useful_information/contact_screen.dart
@@ -1,25 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class TestScreen extends StatefulWidget {
-  const TestScreen({Key? key}) : super(key: key);
+class ContactUSScreen extends StatelessWidget {
+  const ContactUSScreen({Key? key}) : super(key: key);
 
-  @override
-  State<TestScreen> createState() => _TestScreenState();
-}
-
-class _TestScreenState extends State<TestScreen> {
-  int childValue = 0;
-  List<int> expansion = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
+        title: Text("About_Us".tr(),style: Theme.of(context).textTheme.titleSmall,),
+        centerTitle: true,
         iconTheme: Theme.of(context).iconTheme,
-        title: Text(
-          "Test Screen",
-          style: Theme.of(context).textTheme.titleSmall,
-        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -30,7 +23,7 @@ class _TestScreenState extends State<TestScreen> {
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                  style: Theme.of(context).textTheme.labelMedium,
+                    style: Theme.of(context).textTheme.labelMedium,
                   ),
                 ),
               ),
