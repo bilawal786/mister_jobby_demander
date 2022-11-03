30337d1256861e84356adc7ae4b61c15401421d8
mujeebcc <mujeeb@codingcrust.com>
2022-11-03 18:01:50 +0500

    forget password api done

--- lib/screens/auth_screens/forget_password_screen.dart
+++ lib/screens/auth_screens/forget_password_screen.dart
@@ -1,12 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../../providers/auth_provider/forget_password_provider.dart';
-import '../../providers/auth_provider/login_provider.dart';
 import '../../widgets/const_widgets/custom_button.dart';
-import 'confirm_password_screen.dart';
-import 'otp_verify_screen.dart';
 
 class ForgetPassword extends StatefulWidget {
   const ForgetPassword({Key? key}) : super(key: key);
@@ -14,32 +10,28 @@ class ForgetPassword extends StatefulWidget {
   @override
   State<ForgetPassword> createState() => _ForgetPasswordState();
 }
-
 class _ForgetPasswordState extends State<ForgetPassword> {
   final formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   RegExp regExp = RegExp(
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
       multiLine: false);
-
   @override
   void dispose() {
     emailController.dispose();
     super.dispose();
   }
-
-  void formSubmitemail() {
-     final verifyEmailData = Provider.of<ForgetPasswordProvider>(context, listen: false);
+  void formSubmitEmail() {
+    final verifyEmailData =
+        Provider.of<ForgetPasswordProvider>(context, listen: false);
     var isValid = formKey.currentState!.validate();
-
     if (!isValid) {
-      return ;
+      return;
     }
     formKey.currentState!.save();
     verifyEmailData.verifyEmail(context, emailController.text);
-        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ConfirmPassword(email: emailController.text.toString()),));
+    // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ConfirmPassword(email: emailController.text.toString()),));
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,8 +68,7 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                       height: MediaQuery.of(context).size.width / 40,
                     ),
                     TextFormField(
-                      autovalidateMode:
-                      AutovalidateMode.onUserInteraction,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
                       controller: emailController,
                       decoration: InputDecoration(
                         border: const OutlineInputBorder(),
@@ -101,7 +92,8 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                     SizedBox(
                       height: MediaQuery.of(context).size.width / 25,
                     ),
-                    CustomButton(onPress: formSubmitemail,
+                    CustomButton(
+                        onPress: formSubmitEmail,
                         // ()=>
                         // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ConfirmPassword(),),),
                         buttonName: "Confirm Email"),
