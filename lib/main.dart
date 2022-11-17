9c005ad6b127dc7850af9d38a52f2486d66abda1
Hassan <hassan@codingcrust.com>
2022-11-17 18:14:10 +0500

    geocoding plugin removed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/services.dart';
 import 'package:mister_jobby/providers/auth_provider/forget_password_provider.dart';
+import 'package:mister_jobby/providers/coordinates_provider.dart';
 import 'package:mister_jobby/providers/jobs_provider/jobs_compleleted_provider.dart';
 import './providers/jobs_provider/job_proposals_provider.dart';
 import 'package:mister_jobby/screens/accounts_screens/manage_accounts/insurance.dart';
@@ -197,6 +198,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create:(context) => JobsCompletedProvider(),),
         ChangeNotifierProvider(create:(context) => ForgetPasswordProvider(),),
         ChangeNotifierProvider(create:(context) => RatingProvider(),),
+        ChangeNotifierProvider(create:(context) => CoordinateProvider(),),
       ],
       child: MaterialApp(
         localizationsDelegates: context.localizationDelegates,
---
+++ lib/providers/coordinates_provider.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+
+import '../models/coordinatesModel.dart';
+import '../widgets/home_screen_widgets/login_progress_indicator.dart';
+
+class CoordinateProvider with ChangeNotifier {
+  CoordinatesModel? getCoordinates;
+
+  Future<void> getLatLngGeoCodingApi(context, String address) async {
+    showDialog(context: context, builder: (BuildContext context){
+      return const LoginProgressIndicator();
+    });
+    String geoCodingApiKey = "AIzaSyAeKxMwTMJzHH2AR1xt7OLWIWFMIzm-JLM&libraries";
+    String geoCodingBaseUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+    String requestUrl = "$geoCodingBaseUrl?address=$address&key=$geoCodingApiKey";
+    var response = await http.get(Uri.parse(requestUrl));
+    if(response.statusCode == 200){
+
+        getCoordinates = CoordinatesModel.fromJson(jsonDecode(response.body));
+      Navigator.of(context).pop();
+      print("response : ${response.body}");
+      notifyListeners();
+    }else {
+      Navigator.of(context).pop();
+      debugPrint("geoCoding coordinates api is not working");
+      notifyListeners();
+    }
+  }
+}
