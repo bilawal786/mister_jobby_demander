5c55613c4bfee1b1b761db1fa6912b58808f2ee0
Hassan <hassan@codingcrust.com>
2022-11-25 16:09:22 +0500

    transaction screen issue fix

--- lib/screens/accounts_screens/manage_accounts/my_transaction_screen.dart
+++ lib/screens/accounts_screens/manage_accounts/my_transaction_screen.dart
@@ -37,96 +37,88 @@ class _MyTransactionScreenState extends State<MyTransactionScreen> {
         ).tr(),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: RefreshIndicator(
+          onRefresh: () async{
+            await Provider.of<TransactionProvider>(context).getTransaction();
+          },
+          child: ListView.builder(
+            itemCount: extractData!.length,
+            itemBuilder: (context, index) => Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Container(
+                    color: Colors.grey.shade300,
+                    padding: EdgeInsets.all(10),
+                    child:
 
-            // SizedBox(height: MediaQuery.of(context).size.width /40,),
-            RefreshIndicator(
-              onRefresh: () async{
-                await Provider.of<TransactionProvider>(context).getTransaction();
-              },
-              child: ListView.builder(
-                itemCount: extractData!.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: Container(
-                        color: Colors.grey.shade300,
-                        padding: EdgeInsets.all(10),
-                        child:
-
-                        ListTile(
-                          contentPadding: EdgeInsets.only(left: 0,right: 5, top: 5, bottom: 5),
-                          horizontalTitleGap: 10,
-                          leading: Container(
-                            width: MediaQuery.of(context).size.width /7,
-                            height: MediaQuery.of(context).size.width /8,
-                            child: Stack(children: <Widget>[
-                              Center(
-                                child: Container(
-                                    width: MediaQuery.of(context).size.width /10,
-                                    height: MediaQuery.of(context).size.width /10,
-                                    decoration: const BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Colors.black26,),
-                                    child: Icon(FontAwesomeIcons.coins,
-                                      size: MediaQuery.of(context).size.width /20,
-                                      color: Colors.white,)),
-                              ),
-                              Positioned(
-                                  right: -1,
-                                  top: 0,
-                                  child: Icon(
-                                    Icons.arrow_upward,
-                                    size: MediaQuery.of(context).size.width / 25,
-                                    color: Colors.red.shade300,
-                                  ),
-                              ),],),
+                    ListTile(
+                      contentPadding: EdgeInsets.only(left: 0,right: 5, top: 5, bottom: 5),
+                      horizontalTitleGap: 10,
+                      leading: Container(
+                        width: MediaQuery.of(context).size.width /7,
+                        height: MediaQuery.of(context).size.width /8,
+                        child: Stack(children: <Widget>[
+                          Center(
+                            child: Container(
+                                width: MediaQuery.of(context).size.width /10,
+                                height: MediaQuery.of(context).size.width /10,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.black26,),
+                                child: Icon(FontAwesomeIcons.coins,
+                                  size: MediaQuery.of(context).size.width /20,
+                                  color: Colors.white,)),
                           ),
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
+                          Positioned(
+                              right: -1,
+                              top: 0,
+                              child: Icon(
+                                Icons.arrow_upward,
+                                size: MediaQuery.of(context).size.width / 25,
+                                color: Colors.red.shade300,
+                              ),
+                          ),],),
+                      ),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
 
-                              // SizedBox(height: MediaQuery.of(context).size.width /60,),
+                          // SizedBox(height: MediaQuery.of(context).size.width /60,),
+                          Text(
+                            extractData[index].jobTitle,
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                          Row(
+                            children: [
                               Text(
-                                extractData[index].jobTitle,
-                                style: Theme.of(context).textTheme.titleSmall,
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    extractData[index].createdAt,
-                                    style: Theme.of(context).textTheme.labelMedium,
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    "${extractData[index].price} €",
-                                    style: Theme.of(context).textTheme.bodyLarge,
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                extractData[index].invoiceNo,
+                                extractData[index].createdAt,
                                 style: Theme.of(context).textTheme.labelMedium,
+                              ),
+                              const Spacer(),
+                              Text(
+                                "${extractData[index].price} €",
+                                style: Theme.of(context).textTheme.bodyLarge,
                               ),
                             ],
                           ),
-                          subtitle: Text(
-                            extractData[index].transactionId.toString(),
-                            style: Theme.of(context).textTheme.bodySmall,
+                          Text(
+                            extractData[index].invoiceNo,
+                            style: Theme.of(context).textTheme.labelMedium,
                           ),
-                        ),
+                        ],
+                      ),
+                      subtitle: Text(
+                        extractData[index].transactionId.toString(),
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
