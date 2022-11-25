daaa032d50e862d3d4d4198d3817331adb65df41
Hassan <hassan@codingcrust.com>
2022-11-25 17:00:32 +0500

    general step 2.5 button disabled issue fix

--- lib/screens/home_screens/notification_display.dart
+++ lib/screens/home_screens/notification_display.dart
@@ -1,7 +1,6 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import '../../providers/notification_provider/notification_provider.dart';
 
 class NotificationDisplay extends StatefulWidget {
@@ -15,54 +14,9 @@ class _NotificationDisplayState extends State<NotificationDisplay> {
   var _isInit = true;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    FirebaseMessaging.instance.getInitialMessage().then(
-          (message) {
-        print("FirebaseMessaging.instance.getInitialMessage");
-        if (message != null) {
-          print("New Notification");
-          if (message.data['_id'] != null) {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => NotificationDisplay(
-                  // id: message.data['_id'],
-                ),
-              ),
-            );
-          }
-        }
-      },
-    );
-    FirebaseMessaging.onMessage.listen(
-          (message) {
-        print("FirebaseMessaging.onMessage.listen");
-        if (message.notification != null) {
-          print(message.notification!.title);
-          print(message.notification!.body);
-          print("message.data11 ${message.data}");
-          // LocalNotificationService.display(message);
-
-        }
-      },
-    );
-    FirebaseMessaging.onMessageOpenedApp.listen(
-          (message) {
-        print("FirebaseMessaging.onMessageOpenedApp.listen");
-        if (message.notification != null) {
-          print(message.notification!.title);
-          print(message.notification!.body);
-          print("message.data22 ${message.data['_id']}");
-        }
-      },
-    );
-
-  }
-  @override
   void didChangeDependencies() {
     if(_isInit){
-      Provider.of<NotificationProvider>(context).getNotification();
+      Provider.of<NotificationProvider>(context, listen: true).getNotification();
     }
     _isInit = false;
     super.didChangeDependencies();
@@ -71,7 +25,7 @@ class _NotificationDisplayState extends State<NotificationDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    final notificationsData = Provider.of<NotificationProvider>(context,);
+    final notificationsData = Provider.of<NotificationProvider>(context, listen: false);
     final extractNotification = notificationsData.notificationItems;
     return Scaffold(
       appBar: AppBar(
@@ -81,8 +35,8 @@ class _NotificationDisplayState extends State<NotificationDisplay> {
         centerTitle: true,
          iconTheme: Theme.of(context).iconTheme,
       ),
-      body: extractNotification == null ? Center(child: CircularProgressIndicator(),) : Padding(
-        padding: const EdgeInsets.all(15),
+      body: extractNotification == null ? const Center(child: CircularProgressIndicator(),) : Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
         child: RefreshIndicator(
           onRefresh: ()async{
             await Provider.of<NotificationProvider>(context, listen: false).getNotification();
